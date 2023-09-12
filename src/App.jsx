c26884ced3f78ef0cff3165323d9256f052473b5
Luis <luisguibarreto8@gmail.com>
2023-09-12 11:45:20 -0300

    Adicionando aquivos

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+    <div className='container'>
+      <section className='estrutura'>
+        <article className='box'>
+          <h1>Nossa etrutura</h1>
+          <img src="http://lorempixel.com.br/350/200/?1" alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Laudantium molestiae libero omnis magnam recusandae praesentium, qui. 
+          </p>
+          <a href="#">
+            veja mais
+          </a>
+        </article>
+      </section>
+    </div>
+    </>
+  )
+}
+
+export default App
